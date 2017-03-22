f792ebae003727168772e2c389173960aebad012
aspiggy <christie_puppy@yahoo.com>
2017-03-22 10:32:08 +0800

    Delete testPR.ps1

--- testPR.ps1
+++
@@ -1,6 +0,0 @@
-function test($ip,$user,$pw,$cmd) { 
-    Set-Item WSMan:\localhost\Client\TrustedHosts -Value * -Force  // Add all hosts or a single host into the cred list
-    $Password = ConvertTo-SecureString $pw -AsPlainText –Force
-    $cred = New-Object System.Management.Automation.PSCredential($user,$Password) //create a remote credential
-    invoke-command -ComputerName $ip -Credential $cred -ScriptBlock { $cmd }  //incove the cmd
-}
