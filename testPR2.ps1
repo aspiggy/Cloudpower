c5fd742e36f5e7b6db84bd3f16d35d7d378a9a70
aspiggy <christie_puppy@yahoo.com>
2017-03-22 10:31:51 +0800

    Delete testPR2.ps1

--- testPR2.ps1
+++
@@ -1,6 +0,0 @@
-function test($ip,$user,$pw) { 
-    Set-Item WSMan:\localhost\Client\TrustedHosts -Value * -Force 
-    $Password = ConvertTo-SecureString $pw -AsPlainText –Force
-    $cred = New-Object System.Management.Automation.PSCredential($user,$Password)
-    Enter-PSSession -ComputerName $ip -Credential $cred
-}
